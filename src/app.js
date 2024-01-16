5d99d0c4ff2abf2e967f4d5b20f34b862ad93a7f
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-01-16 08:46:27 -0500

    changes made to src files for jwt functionality

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
 
+const passport = require('passport');
+const authenticate = require('./auth');
+
 // author and version from our package.json file
 // TODO: make sure you have updated your name in the `author` section
 const { author, version } = require('../package.json');
@@ -29,6 +32,10 @@ app.use(cors());
 
 // Use gzip/deflate compression middleware
 app.use(compression());
+
+// Set up our passport authentication middleware
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
 
 // Define our routes
 app.use('/', require('./routes'));
