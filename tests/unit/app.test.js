bf3011c0dc3da4a5fc397c52dac1ba53245a5ed1
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-01-23 09:00:21 -0500

    test files completed for Lab3

---
+++ tests/unit/app.test.js
@@ -0,0 +1,11 @@
+// tests/unit/app.test.js
+
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('unavailable route', () => {
+  //if the user tries to access a non-existing route the user should get an http 404 error.
+  test('non-existing route returns 404', () => 
+  request(app).get('/non-existing-route').expect(404));
+});
