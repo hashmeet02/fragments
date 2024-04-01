ece1aa5171558a7f0909fe227d0edfd85cc9413e
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-04-01 08:39:17 -0400

    organized routes for

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -24,20 +24,20 @@ const rawBody = () =>
     },
   });
 
+// Define our first route, which will be: GET /v1/fragments
+router.get('/fragments', require('./get'));
+
+//GET /v1/fragments/:id
+router.get('/fragments/:id', require('./getId'));
+  
+//GET /v1/fragments/:id/info
+router.get('/fragments/:id/info', require('./getIdInfo'));
+
 // Use a raw body parser for POST, which will give a `Buffer` Object or `{}` at `req.body`
 // You can use Buffer.isBuffer(req.body) to test if it was parsed by the raw body parser.
 router.post('/fragments', rawBody(), require('./post'));
 
 // DELETE /v1/fragments
 router.delete('/fragments/:id', require('./delete'));
-
-// Define our first route, which will be: GET /v1/fragments
-router.get('/fragments', require('./get'));
-
-//GET /v1/fragments/:id
-router.get('/fragments/:id', require('./getId'));
-
-//GET /v1/fragments/:id/info
-router.get('/fragments/:id/info', require('./getIdInfo'));
 
 module.exports = router;
